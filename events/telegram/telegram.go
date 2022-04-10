a949b3cac1ed984c1aa1076e3d7391f9deb6c6d2
Nikita <psihachina@gmail.com>
2022-04-10 10:24:16 +0300

    add EventProcessor

---
+++ events/telegram/telegram.go
@@ -0,0 +1,119 @@
+package telegram
+
+import (
+	"errors"
+
+	"github.com/psihachina/telegrambot/clients/telegram"
+	"github.com/psihachina/telegrambot/events"
+	"github.com/psihachina/telegrambot/lib/e"
+	"github.com/psihachina/telegrambot/storage"
+)
+
+type Processor struct {
+	tg      *telegram.Client
+	offset  int
+	storage storage.Storage
+}
+
+type Meta struct {
+	ChatID   int
+	Username string
+}
+
+var (
+	ErrUnknownEventType = errors.New("unknown event type")
+	ErrUnknownMetaType  = errors.New("unknown meta type")
+)
+
+func New(client *telegram.Client, storage storage.Storage) *Processor {
+	return &Processor{
+		tg:      client,
+		storage: storage,
+	}
+}
+
+func (p *Processor) Fetch(limit int) ([]events.Event, error) {
+	updates, err := p.tg.Updates(p.offset, limit)
+	if err != nil {
+		return nil, e.Wrap("can't get events", err)
+	}
+
+	if len(updates) == 0 {
+		return nil, nil
+	}
+
+	res := make([]events.Event, 0, len(updates))
+
+	for _, u := range updates {
+		res = append(res, event(u))
+	}
+
+	p.offset = updates[len(updates)-1].ID + 1
+
+	return res, nil
+}
+
+func (p *Processor) Process(event events.Event) error {
+	switch event.Type {
+	case events.Message:
+		return p.processMessage(event)
+	default:
+		return e.Wrap("can't process message", ErrUnknownEventType)
+	}
+}
+
+func (p *Processor) processMessage(event events.Event) error {
+	meta, err := meta(event)
+	if err != nil {
+		return e.Wrap("can't process message", err)
+	}
+
+	if err := p.doCmd(event.Text, meta.ChatID, meta.Username); err != nil {
+		return e.Wrap("can't process message", err)
+	}
+
+	return nil
+}
+
+func meta(event events.Event) (Meta, error) {
+	res, ok := event.Meta.(Meta)
+	if !ok {
+		return Meta{}, e.Wrap("can't get meta", ErrUnknownMetaType)
+	}
+
+	return res, nil
+}
+
+func event(u telegram.Update) events.Event {
+	uType := fetchType(u)
+
+	res := events.Event{
+		Type: uType,
+		Text: fetchText(u),
+	}
+
+	if uType == events.Message {
+		res.Meta = Meta{
+			ChatID:   u.Message.Chat.ID,
+			Username: u.Message.From.Username,
+		}
+	}
+
+	return res
+}
+
+func fetchType(u telegram.Update) events.Type {
+	if u.Message == nil {
+		return events.Unknown
+	}
+
+	return events.Message
+}
+
+func fetchText(u telegram.Update) string {
+	if u.Message == nil {
+		return ""
+	}
+
+	return u.Message.Text
+}
